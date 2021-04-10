badafd5bbcd20b4e4817bdcd628dfc8a829c3cec
Marko Kustudija <kustudijamarko1986@gmail.com>
2021-04-10 22:48:41 +0200

    adding security

--- Station/src/main/resources/data.sql
+++ Station/src/main/resources/data.sql
@@ -1,4 +1,9 @@
-
+INSERT INTO `user` (id, username, password, role)
+              VALUES (1,'miroslav','$2y$12$NH2KM2BJaBl.ik90Z1YqAOjoPgSd0ns/bF.7WedMxZ54OhWQNNnh6','ADMIN');
+INSERT INTO `user` (id, username, password, role)
+              VALUES (2,'tamara','$2y$12$DRhCpltZygkA7EZ2WeWIbewWBjLE0KYiUO.tHDUaJNMpsHxXEw9Ky','KORISNIK');
+INSERT INTO `user` (id, username, password, role)
+              VALUES (3,'petar','$2y$12$i6/mU4w0HhG8RQRXHjNCa.tG2OwGSVXb0GYUnf8MZUdeadE4voHbC','KORISNIK');
 
 INSERT INTO company(id, name, address, pib) VALUES (1, 'GreyHound', 'Aaa street', 100200300);
 INSERT INTO company(id, name, address, pib) VALUES (2, 'Lasta', 'Bbb street', 400200300);
