4e8c54da5ce938ca2e4636cb0c694c830d0e948b
Marko Kustudija <kustudijamarko1986@gmail.com>
2021-04-10 15:40:06 +0200

    adding data.sql

---
+++ Station/target/classes/data.sql
@@ -0,0 +1,25 @@
+
+
+INSERT INTO company(id, name, address, pib) VALUES (1, 'GreyHound', 'Aaa street', 100200300);
+INSERT INTO company(id, name, address, pib) VALUES (2, 'Lasta', 'Bbb street', 400200300);
+INSERT INTO company(id, name, address, pib) VALUES (3, 'NisExpres', 'Ccc street', 300200300);
+INSERT INTO company(id, name, address, pib) VALUES (4, 'PinginExpres', 'Ddd street', 500200300);
+INSERT INTO company(id, name, address, pib) VALUES (5, 'Solaris', 'Eee street', 600200300);
+
+INSERT INTO timetable(id, number_of_seats, ticket_price, departure_time, destination, company_id) 
+ VALUES (1, 20, 100, '12:00', 'Beograd', 1);
+ INSERT INTO timetable(id, number_of_seats, ticket_price, departure_time, destination, company_id) 
+ VALUES (2, 30, 110, '13:00', 'Novi Sad', 2);
+ INSERT INTO timetable(id, number_of_seats, ticket_price, departure_time, destination, company_id) 
+ VALUES (3, 40, 120, '14:00', 'Podgorica', 3);
+ INSERT INTO timetable(id, number_of_seats, ticket_price, departure_time, destination, company_id) 
+ VALUES (4, 10, 130, '15:00', 'Kopaonik', 4);
+ INSERT INTO timetable(id, number_of_seats, ticket_price, departure_time, destination, company_id) 
+ VALUES (5, 20, 140, '16:00', 'Zlatibor', 5);
+
+INSERT INTO reservation (id, timetable_id) VALUES (1,1);
+INSERT INTO reservation (id, timetable_id) VALUES (2,2);
+INSERT INTO reservation (id, timetable_id) VALUES (3,3);
+INSERT INTO reservation (id, timetable_id) VALUES (4,4);
+INSERT INTO reservation (id, timetable_id) VALUES (5,5);
+
